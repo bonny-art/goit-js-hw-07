183b1c25ff820db7acea7e2b2fad04860a3a7ed6
bonny-art <svitlana.otenko@gmail.com>
2025-02-20 03:39:33 +0200

    Task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,25 @@
+const form = document.querySelector(".login-form");
 
+const handleFormSubmit = (e) => {
+  e.preventDefault();
+  const { email, password } = e.target.elements;
+
+  const emailText = email.value.trim();
+  const passwordText = password.value.trim();
+
+  if (!emailText || !passwordText) {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const formData = {
+    email: emailText,
+    password: passwordText,
+  };
+
+  console.table(formData);
+
+  form.reset();
+};
+
+form.addEventListener("submit", handleFormSubmit);
