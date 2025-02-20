b9caa03abeba999ce18640f0c9c3eb0d66bd226f
bonny-art <svitlana.otenko@gmail.com>
2025-02-20 16:21:33 +0200

    Task-6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,50 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const handleCreateBoxes = () => {
+  const amount = input.value.trim();
+
+  if (amount < 1 || amount > 100) {
+    alert("Please, enter boxes amount from 1 to 100");
+    input.value = "";
+    return;
+  }
+
+  container.innerHTML = createBoxes(amount);
+  input.value = "";
+};
+
+const createBoxes = (amount) => {
+  let markupArr = "";
+  let boxSize = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    const color = getRandomHexColor();
+    markupArr += `<div style="background-color: ${color};width: ${boxSize}px; height: ${boxSize}px;"></div>`;
+
+    boxSize += 10;
+  }
+
+  return markupArr;
+};
+
+const destroyBoxes = () => {
+  container.innerHTML = "";
+};
+
+const handleDestroyBoxes = () => {
+  const isConfirmed = confirm("Are you shure you want to delete all boxes?");
+
+  if (isConfirmed) {
+    destroyBoxes();
+  }
+};
+
+const input = document.querySelector("#controls input");
+const createButton = document.querySelector("button[data-create]");
+const destroyButton = document.querySelector("button[data-destroy]");
+const container = document.querySelector("#boxes");
+
+createButton.addEventListener("click", handleCreateBoxes);
+destroyButton.addEventListener("click", handleDestroyBoxes);
